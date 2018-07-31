ab7dfe63be8a8f55838238d6ae7b5faac6f89de1
alexandermaxs <38385660+alexandermaxs@users.noreply.github.com>
2018-07-31 21:47:12 +0300

    Add files via upload

---
+++ src/main/java/team/test/DBPropertyManagerTest.java
@@ -0,0 +1,28 @@
+package team.test;
+
+import team.service.DBPropertyManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DBPropertyManagerTest {
+    private static final Logger LOGGER = LogManager.getLogger(DBPropertyManagerTest.class);
+    private DBPropertyManager dbPropertyManager;
+
+    @Before
+    public void setUp(){
+        dbPropertyManager = DBPropertyManager.getInstance();
+    }
+
+    @After
+    public void tearDown(){
+        dbPropertyManager = null;
+    }
+
+    @Test
+    public void getInstance() {
+        LOGGER.info(dbPropertyManager.getValue("user"));
+    }
+}
