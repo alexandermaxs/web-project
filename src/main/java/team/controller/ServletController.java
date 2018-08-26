932c460641f0df8f09ddf3485029ce8a7d691a36
alexandermaxs <38385660+alexandermaxs@users.noreply.github.com>
2018-08-26 10:54:33 +0300

    Add files via upload

--- src/main/java/team/controller/ServletController.java
+++ src/main/java/team/controller/ServletController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
 import team.command.Command;
 import team.command.CommandFactory;
 
@@ -12,14 +13,15 @@ import team.command.CommandFactory;
  * This class corresponds MVC
  * This Servlet handles requests
  */
-public class ServletController extends HttpServlet{
+public class ServletController extends HttpServlet {
 
     /**
      * This method handles requests
-     * @param request a httpServletRequest
+     *
+     * @param request  a httpServletRequest
      * @param response a httpServletResponse
      * @throws ServletException a ServletException
-     * @throws IOException an IOException
+     * @throws IOException      an IOException
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -29,10 +31,11 @@ public class ServletController extends HttpServlet{
 
     /**
      * This method handles requests
-     * @param request a httpServletRequest
+     *
+     * @param request  a httpServletRequest
      * @param response a httpServletResponse
      * @throws ServletException a ServletException
-     * @throws IOException an IOException
+     * @throws IOException      an IOException
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -43,10 +46,11 @@ public class ServletController extends HttpServlet{
     /**
      * This method gets a instance of <code>Command</code> from <code>CommandFactory</code>
      * by request and execute this command. Then it gos to next jsp.
-     * @param request a httpServletRequest
+     *
+     * @param request  a httpServletRequest
      * @param response a httpServletResponse
      * @throws ServletException a ServletException
-     * @throws IOException an IOException
+     * @throws IOException      an IOException
      */
     private void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
